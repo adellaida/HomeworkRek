42db9b8192d0057e228421c3d3a7a24b56c02c0c
ADELFINA <m.fevral2017@yandex.ru>
2023-05-29 23:07:03 +0300

    Second

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -1,9 +1,28 @@
-﻿void ShowNums (int num)
-{
-    Console.Write(num + " ");
-    if (num>1) ShowNums(num - 1);
-    
-    
-}
+﻿//void ShowNums (int num)
+//{
+//    Console.Write(num + " ");
+//    if (num>1) ShowNums(num - 1);   
+//}
 
-ShowNums(5);
+//ShowNums(5);
+
+int SumNums (int m, int n) {
+    int sum =0;
+    if (m<n) {
+        while (n>=m) {
+        sum = sum + m;
+        m = m+1;
+    }
+    }
+    else if (m>n) {
+        while (n<=m) {
+        sum = sum + n;
+        n = n+1;
+    }
+    }
+    else if (m==n) {
+        sum = m;
+    }
+    return sum;
+}
+Console.Write(SumNums(1, 15));
